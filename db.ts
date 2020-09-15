a774a5396484509b741432e0abd890583890ce78
Valerii Vasin <vvasin@groupon.com>
2020-09-16 01:09:34 +0200

    chore: use singleton for db

--- db.ts
+++ db.ts
@@ -6,19 +6,19 @@ const { DATABASE_ENDPOINT, DATABASE_KEY } = process.env;
 const DATABASE_ID = "track-the-gym";
 const CONTAINER_ID = "Items";
 
-export async function trackCheckins(checkins: number): Promise<void> {
-  const client = new CosmosClient({
-    endpoint: DATABASE_ENDPOINT,
-    key: DATABASE_KEY,
-  });
+// as recommended:
+// https://docs.microsoft.com/en-us/azure/azure-functions/manage-connections#cosmosclient-code-example-javascript
+const client = new CosmosClient({
+  endpoint: DATABASE_ENDPOINT,
+  key: DATABASE_KEY,
+});
+const container = client.database(DATABASE_ID).container(CONTAINER_ID);
 
+export async function trackCheckins(checkins: number): Promise<void> {
   const item: CheckinItem = { checkins, timestamp: new Date().toISOString() };
 
   try {
-    await client
-      .database(DATABASE_ID)
-      .container(CONTAINER_ID)
-      .items.upsert(item);
+    await container.items.upsert(item);
   } catch (error) {
     console.error("something went wrong", error);
   }
