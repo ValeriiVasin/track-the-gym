815838e07b8bf260254cb68b361b081bb7bb0d97
Valerii Vasin <vvasin@groupon.com>
2020-09-15 22:49:42 +0200

    fix: move client into checkin tracking

--- db.ts
+++ db.ts
@@ -6,13 +6,14 @@ const { DATABASE_ENDPOINT, DATABASE_KEY } = process.env;
 const DATABASE_ID = "track-the-gym";
 const CONTAINER_ID = "Items";
 
-const client = new CosmosClient({
-  endpoint: DATABASE_ENDPOINT,
-  key: DATABASE_KEY,
-});
-
 export async function trackCheckins(checkins: number): Promise<void> {
+  const client = new CosmosClient({
+    endpoint: DATABASE_ENDPOINT,
+    key: DATABASE_KEY,
+  });
+
   const item: CheckinItem = { checkins, timestamp: new Date().toISOString() };
+
   try {
     await client
       .database(DATABASE_ID)
