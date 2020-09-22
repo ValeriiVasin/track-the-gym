c659083dff1c7f2292ebd3376e709d6498d34501
Valerii Vasin <valerii.vasin@gmail.com>
2020-09-23 00:34:12 +0200

    feat: parse date helper

--- packages/ui/src/helpers/parse-date.spec.ts
+++ packages/ui/src/helpers/parse-date.spec.ts
@@ -1,3 +1,24 @@
-describe('hello world', () => {
-  it('just works', () => {});
+import { parseDate } from './parse-date';
+
+describe('parse date', () => {
+  it('truncates everything to minutes', () => {
+    const parsed = parseDate('2020-09-22T22:21:48.598Z');
+
+    expect(parsed.getMilliseconds()).toBe(0);
+    expect(parsed.getSeconds()).toBe(0);
+  });
+
+  it('floors minutes to 00/15/30/45', () => {
+    expect(parseDate('2020-09-22T22:00:48.598Z').getMinutes()).toBe(0);
+    expect(parseDate('2020-09-22T22:14:48.598Z').getMinutes()).toBe(0);
+    expect(parseDate('2020-09-22T22:15:48.598Z').getMinutes()).toBe(15);
+    expect(parseDate('2020-09-22T22:16:48.598Z').getMinutes()).toBe(15);
+    expect(parseDate('2020-09-22T22:29:48.598Z').getMinutes()).toBe(15);
+    expect(parseDate('2020-09-22T22:30:48.598Z').getMinutes()).toBe(30);
+    expect(parseDate('2020-09-22T22:31:48.598Z').getMinutes()).toBe(30);
+    expect(parseDate('2020-09-22T22:44:48.598Z').getMinutes()).toBe(30);
+    expect(parseDate('2020-09-22T22:45:48.598Z').getMinutes()).toBe(45);
+    expect(parseDate('2020-09-22T22:46:48.598Z').getMinutes()).toBe(45);
+    expect(parseDate('2020-09-22T22:59:48.598Z').getMinutes()).toBe(45);
+  });
 });
--- packages/ui/src/helpers/parse-date.ts
+++ packages/ui/src/helpers/parse-date.ts
@@ -1,3 +1,25 @@
-export function parseDate(date: string): Date {
-  return new Date();
+export function parseDate(isoDate: string): Date {
+  const parsed = new Date(isoDate);
+
+  parsed.setMilliseconds(0);
+  parsed.setSeconds(0);
+  parsed.setMinutes(floorMinutes(parsed.getMinutes()));
+
+  return parsed;
+}
+
+function floorMinutes(minutes: number): number {
+  if (minutes < 15) {
+    return 0;
+  }
+
+  if (minutes < 30) {
+    return 15;
+  }
+
+  if (minutes < 45) {
+    return 30;
+  }
+
+  return 45;
 }
