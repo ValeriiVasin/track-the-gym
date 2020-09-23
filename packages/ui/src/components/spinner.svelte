1984a05b4c4d033d286bed06b607971791e7b427
Valerii Vasin <valerii.vasin@gmail.com>
2020-09-23 23:13:19 +0200

    feat: spinner component

---
+++ packages/ui/src/components/spinner.svelte
@@ -0,0 +1,13 @@
+<style lang="scss">
+  .spinner {
+    display: flex;
+    justify-content: center;
+
+    &:before {
+      content: 'Loading...';
+      font-weight: bold;
+    }
+  }
+</style>
+
+<div class="spinner" />
