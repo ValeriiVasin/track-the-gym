0c66d11418415d1b07defb1288f04b3c412a66ea
Valerii Vasin <valerii.vasin@gmail.com>
2020-09-24 23:07:54 +0200

    feat: use whole area for chart

--- packages/ui/src/helpers/render-chart.ts
+++ packages/ui/src/helpers/render-chart.ts
@@ -17,6 +17,14 @@ export async function renderChart(element: HTMLDivElement, dataTable: DataSet) {
       maxValue: 200,
       ticks: [25, 50, 75, 100, 125, 150, 175, 200],
     },
+    chartArea: {
+      height: '100%',
+      width: '100%',
+      top: 48,
+      left: 48,
+      right: 16,
+      bottom: 64,
+    },
   };
 
   new google.visualization.LineChart(element).draw(
